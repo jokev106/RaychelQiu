a8336646421c6d4089ea05560af536c9727a5666
jokev106 <62888793+jokev106@users.noreply.github.com>
2022-10-20 14:17:08 +0700

    RaychelQiu project

---
+++ RaychelQiu/Shared/RaychelQiuApp.swift
@@ -0,0 +1,20 @@
+//
+//  RaychelQiuApp.swift
+//  Shared
+//
+//  Created by Jonathan Kevin on 20/10/22.
+//
+
+import SwiftUI
+
+@main
+struct RaychelQiuApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
