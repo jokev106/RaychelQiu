ed5325bd917393f9e1bb31da0cd9297923cc18fc
vleonard <vleonard@student.ciputra.ac.id>
2022-10-31 14:42:58 +0700

    Add: Scene1 Prologue

--- RaychelQiu/RaychelQiu/RaychelQiuApp.swift
+++ RaychelQiu/RaychelQiu/RaychelQiuApp.swift
@@ -13,7 +13,7 @@ struct RaychelQiuApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Scene_Testing()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
