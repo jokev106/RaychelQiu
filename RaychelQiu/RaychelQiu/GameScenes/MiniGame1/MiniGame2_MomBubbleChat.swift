8e76adef5ebb58ae343a55fee69757400fe51b24
jokev106 <62888793+jokev106@users.noreply.github.com>
2022-11-02 07:54:36 +0700

    New MiniGame file added

---
+++ RaychelQiu/RaychelQiu/GameScenes/MiniGame1/MiniGame2_MomBubbleChat.swift
@@ -0,0 +1,20 @@
+//
+//  MiniGame2_MomBubbleChat.swift
+//  RaychelQiu
+//
+//  Created by Jonathan Kevin on 01/11/22.
+//
+
+import SwiftUI
+
+struct MiniGame2_MomBubbleChat: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MiniGame2_MomBubbleChat_Previews: PreviewProvider {
+    static var previews: some View {
+        MiniGame2_MomBubbleChat()
+    }
+}
