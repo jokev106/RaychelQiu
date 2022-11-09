eab654ca4081b5f5e6a692d9e7a7f12d3c9eb583
jokev106 <62888793+jokev106@users.noreply.github.com>
2022-11-09 14:57:31 +0700

    Update assets

--- RaychelQiu/RaychelQiu/GameScenes/MiniGame1/Minigame1_BackpackSchool.swift
+++ RaychelQiu/RaychelQiu/GameScenes/MiniGame1/Minigame1_BackpackSchool.swift
@@ -9,10 +9,13 @@ import SwiftUI
 
 struct Minigame1_BackpackSchool: View {
     
-    @State var positionBook = CGPoint(x: 72, y: 93)
-    @State var positionPencil = CGPoint(x: 62, y: 163)
-    @State var positionBootle = CGPoint(x: 122, y: 203)
-    @State var positionBag = CGPoint(x:295, y: 153)
+    @State var positionBook = CGPoint(x: 95, y: 593)
+    @State var positionPencil = CGPoint(x: 80, y: 693)
+    @State var positionBootle = CGPoint(x: 155, y: 673)
+    @State var positionBag = CGPoint(x:295, y: 630)
+    @State var positionPlayBox = CGPoint(x: 198, y: 635)
+    @State var kotak = CGPoint(x: 360, y: 635)
+    @State var kotak1 = CGPoint(x: 250, y: 720)
 
     @State var moveableBook: Bool = false
     @State var moveablePencil: Bool = false
@@ -21,6 +24,11 @@ struct Minigame1_BackpackSchool: View {
     @State var book: Bool = false
     @State var pencil: Bool = false
     @State var bottle: Bool = false
+    
+    //Visible item
+    @State var isBook:Bool = true
+    @State var isPencil:Bool = true
+    @State var isBottle:Bool = true
     
     var body: some View {
         //Dragable for Book
@@ -31,10 +39,17 @@ struct Minigame1_BackpackSchool: View {
                 }
             })
                .onEnded {gesture in
-                   if self.positionBook.x > 250 {
+                   if self.positionBook.x > 250 &&
+                        self.positionBook.x < 360 &&
+                        self.positionBook.y > 550 && self.positionBook.y < 720{
                        self.moveableBook = true
                        book = true
-                       
+                       isBook = false
+                   }else {
+                       withAnimation(.default){
+                           self.positionBook.x = 95
+                           self.positionBook.y = 593
+                       }
                    }
                }
         //Dragable for PencilCase
@@ -45,9 +60,17 @@ struct Minigame1_BackpackSchool: View {
             }
         })
            .onEnded {gesture in
-               if self.positionPencil.x > 250 {
+               if self.positionPencil.x > 250 &&
+                    self.positionPencil.x < 360 &&
+                    self.positionPencil.y > 550 && self.positionPencil.y < 720 {
                    self.moveablePencil = true
                    pencil = true
+                   isPencil = false
+               }else {
+                   withAnimation(.default){
+                       self.positionPencil.x = 80
+                       self.positionPencil.y = 693
+                   }
                }
            }
         
@@ -61,62 +84,61 @@ struct Minigame1_BackpackSchool: View {
                 }
             })
                .onEnded {gesture in
-                   if self.positionBootle.x > 250 {
+                   if self.positionBootle.x > 250 &&
+                        self.positionBootle.x < 360 &&
+                        self.positionBootle.y > 550 && self.positionBootle.y < 720 {
                        self.moveableBottle = true
                        bottle = true
-                       
+                       isBottle = false
+                   }else {
+                       withAnimation(.default){
+                           self.positionBootle.x = 155
+                           self.positionBootle.y = 673
+                       }
                    }
                }
         
         GeometryReader{geo in
-            VStack{
-                Spacer()
-                if book == false || pencil == false || bottle == false{
-                    ZStack{
-                        Image("Bag")
-                            .resizable()
-                            .frame(width: 140, height: 190)
-                            .position(self.positionBag)
-                            .clipped()
-                        Image("Books")
-                            .resizable()
-                            .frame(width: 100, height: 90)
-                            .position(self.positionBook)
-                            .gesture(dragBook)
-                            .clipped()
-                            .animation(Animation.default.repeatCount(4).speed(6))
-                        Image("PencilCase")
-                            .resizable()
-                            .frame(width: 80, height: 80)
-                            .position(self.positionPencil)
-                            .gesture(dragPencil)
-                            .clipped()
-                        
-                        Image("WaterBottle")
-                            .resizable()
-                            .frame(width: 40, height: 100)
-                            .position(self.positionBootle)
-                            .gesture(dragBottle)
-                            .clipped()
-                    }.background()
-                        .frame(width: 390, height: 270)
-                        .transition(AnyTransition.opacity.animation(.easeInOut(duration: 0.8)))
-                }
-                if book == true && pencil == true && bottle == true{
-                    Image(systemName: "chevron.right.circle")
+            ZStack{
+                Image("PlayBox")
+                    .resizable()
+                    .frame(width: 330, height: 224)
+                    .position(self.positionPlayBox)
+                
+//                    ZStack{
+                Image("Bag")
+                    .resizable()
+                    .frame(width: 140, height: 190)
+                    .position(self.positionBag)
+                    .clipped()
+                
+                if isBook == true {
+                    Image("Books")
                         .resizable()
-                        .foregroundColor(.black)
+                        .frame(width: 100, height: 90)
+                        .position(self.positionBook)
+                        .gesture(dragBook)
+                        .clipped()
+                }
+                if isPencil == true {
+                    Image("PencilCase")
+                        .resizable()
                         .frame(width: 80, height: 80)
-                        .onTapGesture {
-                            withAnimation(.default) {
-                                
-                            }
-                        }
-                        .transition(AnyTransition.opacity.animation(.easeInOut(duration: 0.8)))
+                        .position(self.positionPencil)
+                        .gesture(dragPencil)
+                        .clipped()
+                    
+                }
+                if isBottle == true {
+                    Image("WaterBottle")
+                        .resizable()
+                        .frame(width: 40, height: 100)
+                        .position(self.positionBootle)
+                        .gesture(dragBottle)
+                        .clipped()
                 }
             }
         }
-
     }
 }
 struct Minigame1_BackpackSchool_Previews: PreviewProvider {
