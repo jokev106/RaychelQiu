961584d8f204163ed7628aeb16c1228b708b1d6d
sirjvp <jonathanvalentinoprawira@yahoo.com>
2022-11-09 15:58:28 +0700

    Update: Game 5 Border

--- RaychelQiu/RaychelQiu/GameScenes/Game5/Game5View.swift
+++ RaychelQiu/RaychelQiu/GameScenes/Game5/Game5View.swift
@@ -23,33 +23,37 @@ struct Game5View: View {
     var body: some View {
         GeometryReader { _ in
             ZStack {
-                VStack {
+                VStack(spacing: 0) {
                     ZStack {
+                        Image("Game_Border")
+                            .resizable()
+//                            .scaledToFit()
+                            .frame(width: 330, height: 449.6)
+                            .position(x: 198, y: 275)
+                        
                         Group {
                             ForEach(0 ..< 20, id: \.self) { index in
                                 Image(parts[index].image)
                                     .renderingMode(.template)
                                     .resizable()
                                     .scaledToFit()
-                                    .foregroundColor( colors[changeColor[index]])
-//                            Image("Game5_3")
-//                                .resizable()
-//                                .scaledToFit()
-//                                .overlay(
-//                                    Rectangle()
-//                                        .foregroundColor(changeColor[0] == 1 ? .red : changeColor[0] == 2 ? .yellow : changeColor[0] == 3 ? .blue : .gray)
-//                                        .mask(
-//                                            Image("Game5_3")
-//                                                .resizable()
-//                                                .scaledToFit()
-//                                        )
-//                                        .onTapGesture {
-//                                            changeColor[0] = colorSelected
-//                                        }
+                                    .foregroundColor(colors[changeColor[index]])
+                                //                            Image("Game5_3")
+                                //                                .resizable()
+                                //                                .scaledToFit()
+                                //                                .overlay(
+                                //                                    Rectangle()
+                                //                                        .foregroundColor(changeColor[0] == 1 ? .red : changeColor[0] == 2 ? .yellow : changeColor[0] == 3 ? .blue : .gray)
+                                //                                        .mask(
+                                //                                            Image("Game5_3")
+                                //                                                .resizable()
+                                //                                                .scaledToFit()
+                                //                                        )
+                                //                                        .onTapGesture {
+                                //                                            changeColor[0] = colorSelected
+                                //                                        }
                             }
-                        }
-                        
-                        Group {
+                            
                             ForEach(0 ..< 20, id: \.self) { index in
                                 Button(action: {
                                     changeColor[index] = colorSelected
@@ -61,20 +65,21 @@ struct Game5View: View {
                                 .rotationEffect(.degrees(parts[index].rotation))
                                 .position(x: parts[index].x, y: parts[index].y)
                             }
+                            
+                            Image("Game5_Outline")
+                                .resizable()
+                                .scaledToFit()
+                                .allowsHitTesting(false)
+                                .opacity(done ? 0.0 : 1.0)
+                            
+                            Image("Game5_Numbers")
+                                .resizable()
+                                .scaledToFit()
+                                .allowsHitTesting(false)
+                                .opacity(done ? 0.0 : 1.0)
                         }
-                        
-                        Image("Game5_Outline")
-                            .resizable()
-                            .scaledToFit()
-                            .allowsHitTesting(false)
-                            .opacity(done ? 0.0 : 1.0)
-                        
-
-                        Image("Game5_Numbers")
-                            .resizable()
-                            .scaledToFit()
-                            .allowsHitTesting(false)
-                            .opacity(done ? 0.0 : 1.0)
+                        .scaleEffect(0.8)
+                        .offset(y: 80)
                     }
                     .onChange(of: changeColor) { _ in
                         var count = 0
@@ -93,21 +98,28 @@ struct Game5View: View {
                         }
                     }
                     
-                    HStack {
-                        ForEach(0 ..< 5, id: \.self) { index in
-                            Image("Game5_Picker\(index + 1)")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 60)
-                                .offset(y: colorSelected == index + 1 ? -40 : 0)
-                                .onTapGesture {
-                                    colorSelected = index + 1
-                                }
+                    ZStack {
+                        Image("Game_Border")
+                            .resizable()
+                            .frame(width: 330, height: 200)
+                            .position(x: 198, y: 285)
+                        
+                        HStack {
+                            ForEach(0 ..< 5, id: \.self) { index in
+                                Image("Game5_Picker\(index + 1)")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 50)
+                                    .offset(y: colorSelected == index + 1 ? -40 : 0)
+                                    .onTapGesture {
+                                        colorSelected = index + 1
+                                    }
+                            }
                         }
+                        .offset(y: 120)
                     }
-                    .padding()
+                    .offset(y: -40)
                 }
-                .background(.blue)
             }
         }
     }
