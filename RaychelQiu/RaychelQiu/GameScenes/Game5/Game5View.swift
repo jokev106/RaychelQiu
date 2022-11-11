aa05490fdc8293c73dda222fca1d673ffeb12c9f
sirjvp <jonathanvalentinoprawira@yahoo.com>
2022-11-11 15:14:41 +0700

    Fix: Game 5 Change Color

--- RaychelQiu/RaychelQiu/GameScenes/Game5/Game5View.swift
+++ RaychelQiu/RaychelQiu/GameScenes/Game5/Game5View.swift
@@ -9,9 +9,7 @@ import SwiftUI
 
 struct Game5View: View {
     @State var colorSelected = 1
-//    @State var arrayColor = [Color]()
-//    @State var arrayColor = [3, 3, 4, 4, 5, 3, 1, 1, 2, 2, 2, 3, 4, 3, 4, 3, 1, 2, 1, 2]
-//
+
     @State var changeColor = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     
     @State var done = false
@@ -23,103 +21,99 @@ struct Game5View: View {
     var body: some View {
         GeometryReader { _ in
             ZStack {
-                VStack(spacing: 0) {
-                    ZStack {
-                        Image("Game_Border")
+                // Canvas
+                Image("Game_Border")
+                    .resizable()
+                    .frame(width: 330, height: 449.6)
+                    .position(x: 198, y: 275)
+                        
+                Group {
+                    ForEach(0 ..< 20, id: \.self) { index in
+                        Image(parts[index].image)
+                            .renderingMode(.template)
                             .resizable()
-//                            .scaledToFit()
-                            .frame(width: 330, height: 449.6)
-                            .position(x: 198, y: 275)
-                        
-                        Group {
-                            ForEach(0 ..< 20, id: \.self) { index in
-                                Image(parts[index].image)
-                                    .renderingMode(.template)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .foregroundColor(colors[changeColor[index]])
-                                //                            Image("Game5_3")
-                                //                                .resizable()
-                                //                                .scaledToFit()
-                                //                                .overlay(
-                                //                                    Rectangle()
-                                //                                        .foregroundColor(changeColor[0] == 1 ? .red : changeColor[0] == 2 ? .yellow : changeColor[0] == 3 ? .blue : .gray)
-                                //                                        .mask(
-                                //                                            Image("Game5_3")
-                                //                                                .resizable()
-                                //                                                .scaledToFit()
-                                //                                        )
-                                //                                        .onTapGesture {
-                                //                                            changeColor[0] = colorSelected
-                                //                                        }
-                            }
-                            
-                            ForEach(0 ..< 20, id: \.self) { index in
-                                Button(action: {
-                                    changeColor[index] = colorSelected
-                                }) {
-                                    Rectangle()
-                                        .opacity(0.0)
-                                }
-                                .frame(width: parts[index].width, height: parts[index].height)
-                                .rotationEffect(.degrees(parts[index].rotation))
-                                .position(x: parts[index].x, y: parts[index].y)
-                            }
-                            
-                            Image("Game5_Outline")
-                                .resizable()
-                                .scaledToFit()
-                                .allowsHitTesting(false)
-                                .opacity(done ? 0.0 : 1.0)
-                            
-                            Image("Game5_Numbers")
-                                .resizable()
-                                .scaledToFit()
-                                .allowsHitTesting(false)
-                                .opacity(done ? 0.0 : 1.0)
-                        }
-                        .scaleEffect(0.8)
-                        .offset(y: 80)
+                            .scaledToFit()
+                            .foregroundColor(colors[changeColor[index]])
+                        //                            Image("Game5_3")
+                        //                                .resizable()
+                        //                                .scaledToFit()
+                        //                                .overlay(
+                        //                                    Rectangle()
+                        //                                        .foregroundColor(changeColor[0] == 1 ? .red : changeColor[0] == 2 ? .yellow : changeColor[0] == 3 ? .blue : .gray)
+                        //                                        .mask(
+                        //                                            Image("Game5_3")
+                        //                                                .resizable()
+                        //                                                .scaledToFit()
+                        //                                        )
+                        //                                        .onTapGesture {
+                        //                                            changeColor[0] = colorSelected
+                        //                                        }
                     }
-                    .onChange(of: changeColor) { _ in
-                        var count = 0
-                        for index in 0 ..< changeColor.count {
-                            if changeColor[index] == parts[index].color {
-                                count += 1
-                            }
+                            
+                    ForEach(0 ..< 20, id: \.self) { index in
+                        Button(action: {
+                            changeColor[index] = colorSelected
+                        }) {
+                            Rectangle()
+                                .opacity(0.0)
                         }
-                        
-                        if count == 20 {
-                            DispatchQueue.main.asyncAfter(deadline: .now()) {
-                                withAnimation(.easeInOut(duration: 2)) {
-                                    done.toggle()
-                                }
-                            }
-                        }
+                        .frame(width: parts[index].width, height: parts[index].height)
+                        .rotationEffect(.degrees(parts[index].rotation))
+                        .position(x: parts[index].x, y: parts[index].y)
                     }
-                    
-                    ZStack {
-                        Image("Game_Border")
-                            .resizable()
-                            .frame(width: 330, height: 200)
-                            .position(x: 198, y: 285)
-                        
-                        HStack {
-                            ForEach(0 ..< 5, id: \.self) { index in
-                                Image("Game5_Picker\(index + 1)")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 50)
-                                    .offset(y: colorSelected == index + 1 ? -40 : 0)
-                                    .onTapGesture {
-                                        colorSelected = index + 1
-                                    }
-                            }
-                        }
-                        .offset(y: 120)
-                    }
-                    .offset(y: -40)
+                    .offset(y: 70)
+                            
+                    Image("Game5_Outline")
+                        .resizable()
+                        .scaledToFit()
+                        .allowsHitTesting(false)
+                        .opacity(done ? 0.0 : 1.0)
+                            
+                    Image("Game5_Numbers")
+                        .resizable()
+                        .scaledToFit()
+                        .allowsHitTesting(false)
+                        .opacity(done ? 0.0 : 1.0)
                 }
+                .scaleEffect(0.8)
+                .offset(y: -100)
+                .onChange(of: changeColor) { _ in
+                    var count = 0
+                    for index in 0 ..< changeColor.count {
+                        if changeColor[index] == parts[index].color {
+                            count += 1
+                        }
+                    }
+                            
+                    if count == 20 {
+                        DispatchQueue.main.asyncAfter(deadline: .now()) {
+                            withAnimation(.easeInOut(duration: 2)) {
+                                done.toggle()
+                            }
+                        }
+                    }
+                    print(changeColor)
+                }
+                
+                // Color Picker
+                Image("Game_Border")
+                    .resizable()
+                    .frame(width: 330, height: 200)
+                    .position(x: 198, y: 635)
+                        
+                HStack {
+                    ForEach(0 ..< 5, id: \.self) { index in
+                        Image("Game5_Picker\(index + 1)")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50)
+                            .offset(y: colorSelected == index + 1 ? -40 : 0)
+                            .onTapGesture {
+                                colorSelected = index + 1
+                            }
+                    }
+                }
+                .offset(y: 280)
             }
         }
     }
