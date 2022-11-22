fbed14016df4a13dc6a3c31c559ee4e9735b5c6a
vleonard <vleonard@student.ciputra.ac.id>
2022-11-22 15:37:52 +0700

    Add: Scene3 Later Half

---
+++ RaychelQiu/RaychelQiu/Scenes/Chapter 3/Chapter3_Collection.swift
@@ -0,0 +1,20 @@
+//
+//  Chapter3_Collection.swift
+//  RaychelQiu
+//
+//  Created by Vincent Leonard on 17/11/22.
+//
+
+import SwiftUI
+
+struct Chapter3_Collection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Chapter3_Collection_Previews: PreviewProvider {
+    static var previews: some View {
+        Chapter3_Collection()
+    }
+}
