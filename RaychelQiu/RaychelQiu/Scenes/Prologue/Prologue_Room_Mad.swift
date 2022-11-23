0740604b5ad2e8e465d2805c5c8d6ac39fc61c3d
jokev106 <62888793+jokev106@users.noreply.github.com>
2022-11-23 15:26:21 +0700

    Update icon + game scene regis NYFW

--- RaychelQiu/RaychelQiu/Scenes/Prologue/Prologue_Room_Mad.swift
+++ RaychelQiu/RaychelQiu/Scenes/Prologue/Prologue_Room_Mad.swift
@@ -11,6 +11,7 @@ struct Prologue_Room_Mad: View {
     @Binding var mainOnTap: Bool
     @State var onTap = false
     @State var scene = 2
+    //start from scene 2
     
     //Scene1
     @State var door_scale = 1.0
@@ -33,6 +34,33 @@ struct Prologue_Room_Mad: View {
     @State var raychel_papers_opac = 1.0
     @State var raychel_laptop_opac = 0.0
     
+    //Game Register
+    @State var positionRegisBoxMask1 = CGPoint(x: 194, y: 635)
+    @State var positionRegisBoxMask2 = CGPoint(x: 195, y: 385)
+    @State var positionRegisBox = CGPoint(x: 198, y: 635)
+    @State var positionRegisLaptop = CGPoint(x: 204, y: 685)
+    @State var positionRegisButton = CGPoint(x: 198, y: 625)
+    @State var positionInfoButton = CGPoint(x: 148, y: 668)
+    @State var positionTimeButton = CGPoint(x: 261, y: 668)
+    @State var positionBackButton = CGPoint(x: 81, y: 548)
+    @State var positionDoneRegisButton = CGPoint(x: 198, y: 668)
+    
+    @State var infoOpac = 0.0
+    @State var regisOpac = 0.0
+    @State var homeOpac = 1.0
+    @State var timelineOpac = 0.0
+    @State var doneRegisOpac = 0.0
+    
+    //Bool Laptop Screen
+    @State var isHome:Bool = true
+    @State var isInfo:Bool = false
+    @State var isRegis:Bool = false
+    @State var isTimeline:Bool = false
+    @State var isDoneRegis:Bool = false
+//
+//    @State var laptop_x = 0.0
+    @State var laptop_y = 0.0
+    
     var body: some View {
         GeometryReader{geometry in
             ZStack{
@@ -41,7 +69,6 @@ struct Prologue_Room_Mad: View {
                     .scaledToFit()
                     .frame(width: 330, height: 449.6)
                     .offset(y: -114)
-                
                 
                 ZStack{
                     Image("Prologue_Door_BG")
@@ -165,6 +192,201 @@ struct Prologue_Room_Mad: View {
                 .scaleEffect(geometry.size.width * 0.00324)
                 .offset(y: geometry.size.height * 0.011)
             }
+            ZStack{
+                Image("RaychelBorder")
+                    .resizable()
+                    .frame(width: 330, height: 224)
+                    .position(self.positionRegisBox)
+                Image("DInnerBG")
+                    .resizable()
+                    .frame(width: 322, height: 219)
+                    .position(self.positionRegisBox)
+                if scene == 4 {
+                    ZStack{
+                        Image("homescreen")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: geometry.size.width * 1.144)
+                            .opacity(homeOpac)
+                            .position(self.positionRegisLaptop)
+//                            .offset(y: laptop_y)
+                        Image("information")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: geometry.size.width * 1.144)
+                            .opacity(infoOpac)
+                            .position(self.positionRegisLaptop)
+                        Image("register")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: geometry.size.width * 1.144)
+                            .opacity(regisOpac)
+                            .position(self.positionRegisLaptop)
+                        Image("doneregis")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: geometry.size.width * 1.144)
+                            .opacity(doneRegisOpac)
+                            .position(self.positionRegisLaptop)
+//                            .offset(y: laptop_y)
+                        Image("timeline")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: geometry.size.width * 1.144)
+                            .opacity(timelineOpac)
+                            .position(self.positionRegisLaptop)
+                    }
+                    .mask {
+                        Image("Day")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 470, height: 1200)
+                            .position(self.positionRegisBoxMask1)
+                    }.position(self.positionRegisBoxMask2)
+                }
+                
+                //HomeScreen Button
+                Group{
+                    Button {
+                        //Regis
+                        if isHome == true {
+                            isHome = false
+                            isRegis = true
+                            DispatchQueue.main.asyncAfter(deadline: .now()) {
+                                withAnimation(.easeInOut(duration: 1.0)) {
+                                    homeOpac -= 1.0
+                                    regisOpac += 1.0
+                                }
+                            }
+                        }
+                    } label: {
+                        Text("Regis")
+                            .opacity(0)
+                    }.frame(width: 100,height: 30)
+//                        .background(.red)
+                        .position(self.positionRegisButton)
+
+                    
+                    Button {
+                        //Information
+                        if isHome == true {
+                            isHome = false
+                            isInfo = true
+                            DispatchQueue.main.asyncAfter(deadline: .now()) {
+                                withAnimation(.easeInOut(duration: 1.0)) {
+                                    homeOpac -= 1.0
+                                    infoOpac += 1.0
+                                }
+                            }
+                        }
+                    } label: {
+                        Text("Info")
+                            .opacity(0)
+                    }.frame(width: 100,height: 30)
+//                        .background(.red)
+                        .position(self.positionInfoButton)
+                    
+                    Button {
+                        //Timeline
+                        if isHome == true {
+                            isHome = false
+                            isTimeline = true
+                            DispatchQueue.main.asyncAfter(deadline: .now()) {
+                                withAnimation(.easeInOut(duration: 1.0)) {
+                                    homeOpac -= 1.0
+                                    timelineOpac += 1.0
+                                }
+                            }
+                        }
+                    } label: {
+                        Text("Timeline")
+                            .opacity(0)
+                    }.frame(width: 100,height: 30)
+//                        .background(.red)
+                        .position(self.positionTimeButton)
+                }
+                    
+                //Back to Homescreen Button
+                Group{
+                    if isRegis == true || isInfo == true || isTimeline == true {
+                        Button {
+                            //Regis Back to Home
+                            if isRegis == true {
+                                isHome = true
+                                isRegis = false
+                                DispatchQueue.main.asyncAfter(deadline: .now()) {
+                                    withAnimation(.easeInOut(duration: 1.0)) {
+                                        homeOpac += 1.0
+                                        regisOpac -= 1.0
+                                    }
+                                }
+                            }
+                            
+                            //Information back to home
+                            if isInfo == true {
+                                isHome = true
+                                isInfo = false
+                                DispatchQueue.main.asyncAfter(deadline: .now()) {
+                                    withAnimation(.easeInOut(duration: 1.0)) {
+                                        homeOpac += 1.0
+                                        infoOpac -= 1.0
+                                    }
+                                }
+                            }
+                            
+                            //Timeline back to home
+                            if isTimeline == true {
+                                isHome = true
+                                isTimeline = false
+                                DispatchQueue.main.asyncAfter(deadline: .now()) {
+                                    withAnimation(.easeInOut(duration: 1.0)) {
+                                        homeOpac += 1.0
+                                        timelineOpac -= 1.0
+                                    }
+                                }
+                            }
+                            
+                        } label: {
+                            Text("Back")
+                                .font(Font.custom("Hansip", size: 12))
+                                .foregroundColor(.yellow)
+                        }.frame(width: 80,height: 30)
+                            .position(self.positionBackButton)
+                    }
+                    if isRegis == true {
+                        Button {
+                            //RegisNow/Done Regis
+                            if isRegis == true {
+                                isDoneRegis = true
+                                isRegis = false
+                                DispatchQueue.main.asyncAfter(deadline: .now()) {
+                                    withAnimation(.easeInOut(duration: 1.0)) {
+                                        regisOpac -= 1.0
+                                        doneRegisOpac += 1.0
+                                    }
+                                }
+                            }
+                        } label: {
+                            Text("RegisNow")
+                                .opacity(0)
+                        }.frame(width: 100,height: 30)
+//                            .background(.red)
+                            .position(self.positionTimeButton)
+                    }
+                    if isDoneRegis == true {
+                        Button {
+                            // To be Continued
+                            
+                        } label: {
+                            Text("Done")
+                                .font(Font.custom("Hansip", size: 12))
+                                .foregroundColor(.yellow)
+                        }.frame(width: 100,height: 30)
+                        .position(self.positionDoneRegisButton)
+
+                    }
+                }
+            }.offset(y: laptop_y)
             
             
         }
@@ -220,7 +442,7 @@ struct Prologue_Room_Mad: View {
     }
     
     func prologue_room_scene4_out(){
-        raychel_normal_opac = 1.0
+//        raychel_normal_opac = 1.0
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
             withAnimation(.easeInOut(duration: 1.0)) {
@@ -230,10 +452,12 @@ struct Prologue_Room_Mad: View {
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
             withAnimation(.easeInOut(duration: 2.0)) {
-                nyfw_scale += 0.45
-                nyfw_y -= 95
-                nyfw_blur += 5.0
-                raychel_normal_opac += 1.0
+                nyfw_scale = 0.0
+                nyfw_y = 0.0
+                nyfw_blur = 0.0
+                raychel_normal_opac = 1.0
+                laptop_y = 0.0
+//                homeOpac += 1.0
             }
         }
     }
